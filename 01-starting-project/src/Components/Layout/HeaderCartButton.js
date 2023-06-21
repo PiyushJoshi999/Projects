8ae02417f25bacec4104cc75410e5a954a60089f
PiyushJoshi999 <piyushjoshi470@gmail.com>
2023-06-21 17:52:39 +0530

    Adding MealsSummry and AvailableMeals

---
+++ 01-starting-project/src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+
+    return (
+        <button className={classes.button} >
+            <span>
+                Your Cart
+            </span>
+            <span className={classes.badge} >
+                0
+            </span>
+        </button>
+    );
+
+}
+
+export default HeaderCartButton;
