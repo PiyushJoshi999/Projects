a6a5236be310f877c9a6d5835d2d99dcc4ee5ef6
PiyushJoshi999 <piyushjoshi470@gmail.com>
2023-06-27 18:00:50 +0530

    Adding Cart component

--- 01-starting-project/src/Components/Meals/MealSummary.js
+++ 01-starting-project/src/Components/Meals/MealSummary.js
@@ -4,7 +4,7 @@ const MealSummary = () => {
 
     return (
         <section className={classes.summary}>
-        <h2>Delicious Food, Delivered To You!!</h2>
+        <h2>Delicious Food on your Doors!!</h2>
         <p>
           Choose your favorite meal from our broad selection of available meals
           and enjoy a delicious meal at home.
